1fc7dd3ff7b6b8419cd062f00fc787ebaaa0baac
jeffrey <jeffrey.nasson@gmail.com>
2019-01-28 10:42:32 -0700

    wireframe and server setup

---
+++ server/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const massive = require('massive');
+const ctrl = require('./controller.js');
+
+let {SERVER_PORT,CONNECTION_STRING,SESSION_SECRET} = process.env;
+
+const app = express();
+app.use(express.static(__dirname+'/../build'))
+app.use(session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET
+}))
+
+massive(CONNECTION_STRING).then(db=>{
+    app.set('db',db)
+    console.log(`db connected using massive`)
+})
+
+
+app.listen(SERVER_PORT,()=>{
+    console.log(`Listening on Port: ${SERVER_PORT}`)
+})
