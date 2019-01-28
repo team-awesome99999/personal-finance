fa4cb970bea04eab2110d5eab93469dff7d8bc36
mobydickens <steadmegan@gmail.com>
2019-01-28 15:01:43 -0700

    added front end signup

--- src/Pages/Landing Page/Landing.js
+++ src/Pages/Landing Page/Landing.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import './LandingPage.css';
 import { Carousel } from 'react-bootstrap'
+import axios from 'axios';
 
 class Landing extends Component {
+  state = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: ''
+  }
+
+  signup = async () => {
+    const { firstname, lastname, email, password } = this.state;
+    let res = axios.post('/auth/signup', { firstname, lastname, email, password })
+    console.log(res.data);
+  }
+
   render() {
     return (
       <div className='landing-main'>
@@ -17,30 +31,42 @@ class Landing extends Component {
         </Carousel>
         <div>
           <label htmlFor="">First Name</label>
-          <input type="text"/>
+          <input 
+            value={this.state.firstname}
+            onChange={ (e) => this.setState({ firstname: e.target.value }) }
+            type="text"/>
           <label htmlFor="">Last Name</label>
-          <input type="text"/>
+          <input 
+            value={this.state.lastname}
+            onChange={ (e) => this.setState({ lastname: e.target.value }) }
+            type="text"/>
           <label htmlFor="">Email Address</label>
-          <input type="text"/>
+          <input 
+            value={this.state.email}
+            onChange={ (e) => this.setState({ email: e.target.value }) }
+            type="text"/>
           <label htmlFor="">Password</label>
-          <input type="text"/>
-          <button>Register</button>
+          <input 
+            value={this.state.password}
+            onChange={ (e) => this.setState({ password: e.target.value }) }
+            type="text"/>
+          <button onClick={ () => this.signup() }>Register</button>
         </div>
         <div>
           <div>
-            <i class="far fa-money-bill-alt"></i>
+            <i className="far fa-money-bill-alt"></i>
             <h4>See Your Numbers</h4>
           </div>
           <div>
-            <i class="fas fa-chart-line"></i>
+            <i className="fas fa-chart-line"></i>
             <h4>See the Changes</h4>
           </div>
           <div>
-            <i class="fas fa-skiing"></i>
+            <i className="fas fa-skiing"></i>
             <h4>Plan Your Future</h4>
           </div>
           <div>
-            <i class="fas fa-check"></i>
+            <i className="fas fa-check"></i>
             <h4>Done!</h4>
           </div>
         </div>
