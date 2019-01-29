619cc9c0927ba88e11c348848042ef3885d72522
mobydickens <steadmegan@gmail.com>
2019-01-29 11:24:06 -0700

    added action creator get account

--- src/dux/reducer.js
+++ src/dux/reducer.js
@@ -1,10 +1,12 @@
 const initialState={
-  userid: ''
+  userid: '',
+  accountBalance: {},
+  accountDates: {}
 }
 
 // ACTION CONSTANTS
 const GET_CURRENT_USER = 'GET_CURRENT_USER';
-
+const GET_ACCOUNT = 'GET ACCOUNT';
 
 // ACTION CREATORS
 export function getCurrentUser({ userid }) {
@@ -14,11 +16,23 @@ export function getCurrentUser({ userid }) {
   }
 }
 
+export function getAccount({ balances, entrydate }) {
+  return {
+    type: GET_ACCOUNT,
+    payload: {
+      balances,
+      entrydate
+    }
+  }
+}
+
 //REDUCER
 function reducer(state=initialState,action){
    switch(action.type){
       case GET_CURRENT_USER:
         return { ...state, userid: action.payload}
+      case GET_ACCOUNT:
+        return { ...state, accountBalance: this.payload.balances, accountDates: this.payload.entrydate };
       default: return state
    }
 }
