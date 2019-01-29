73d2e6996e7918b4564ae64ff75bb1b2850ad7f0
mobydickens <steadmegan@gmail.com>
2019-01-29 00:24:09 -0700

    slightly styled icon component

--- src/Pages/Landing Page/Icons.js
+++ src/Pages/Landing Page/Icons.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 function Icons() {
   return (
-    <div>
+    <div className='icon-box'>
       <div>
-        <i className="far fa-money-bill-alt"></i>
+        <i className="icons far fa-money-bill-alt"></i>
         <h4>See Your Numbers</h4>
       </div>
       <div>
-        <i className="fas fa-chart-line"></i>
+        <i className="icons fas fa-chart-line"></i>
         <h4>See the Changes</h4>
       </div>
       <div>
-        <i className="fas fa-skiing"></i>
+        <i className="icons fas fa-skiing"></i>
         <h4>Plan Your Future</h4>
       </div>
       <div>
-        <i className="fas fa-check"></i>
-        <h4>Done!</h4>
+        <i className="icons icons-green fas fa-check"></i>
+        <h4>Success!</h4>
       </div>
     </div>
   )
