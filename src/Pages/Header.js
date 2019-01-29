d20180fe5bf37dc8454314fcdfe9b95d127268d3
mobydickens <steadmegan@gmail.com>
2019-01-28 21:55:01 -0700

    broke login functionality into seperate component

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,38 +1,20 @@
 import React, { Component } from 'react';
-import { Navbar, Button, FormGroup, FormControl, Nav, NavItem, Tooltip } from 'react-bootstrap';
-import axios from 'axios';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import Login from './Landing Page/Login'
 import { withRouter } from 'react-router-dom'
 
 class Header extends Component {
 
   state = {
     showLogin: false,
-    email: '',
-    password: '',
-    emailValidation: null,
-    passwordValidation: null
   }
 
-  login = async () => {
-    const { email, password } = this.state;
-    try {
-      let res = await axios.post('/auth/login', { email, password })
-      console.log(res.data)
-      this.setState({
-        email: '',
-        password: ''
-      })
-    } catch(error) {
-      if(error.response.data.message === 'Email not found') {
-        this.setState({ emailValidation: 'error' })
-      } else if(error.response.data.message === 'Incorrect Password') {
-        this.setState({ passwordValidation: 'error' })
-      }
+  showLogin = () => {
+    this.setState({
+      showLogin: !this.state.showLogin
+    })
   }
-    //ONCE HOME PAGE IS BUILD THE LOGIN CAN PUSH THERE WITH COMMENT BELOW
-    // this.props.history.push('/home');
-  }
-  
+
   render() {
     return (
       <div>
@@ -41,40 +23,10 @@ class Header extends Component {
             <Navbar.Brand>Financial Stuff</Navbar.Brand>
           </Navbar.Header>
           {this.state.showLogin ?
-          <Navbar.Form pullRight>
-            <FormGroup validationState={this.state.emailValidation}>
-              <FormControl
-                autoFocus
-                value={ this.state.email }
-                onChange={ (e) => this.setState({ email: e.target.value, emailValidation: null }) } 
-                type="text" 
-                placeholder="Email" />
-                {/* error handling */}
-              { this.state.emailValidation === 'error' ? 
-                <Tooltip placement="bottom" className="in" id="tooltip-bottom">Email not found</Tooltip> : null }
-              <FormControl.Feedback/>
-            </FormGroup>{' '}
-            <FormGroup validationState={this.state.passwordValidation}>
-              <FormControl
-                value={ this.state.password }
-                onChange={ (e) => this.setState({ password: e.target.value, passwordValidation: null }) }  
-                type="text" 
-                placeholder="Password" />
-                {/* error handling */}
-              { this.state.passwordValidation === 'error' ? 
-                <Tooltip placement="bottom" className="in" id="tooltip-bottom">Incorrect password</Tooltip> : null }
-              <FormControl.Feedback/>
-            </FormGroup>{' '}
-            <Button
-              onClick={ () => this.login() }
-              type='submit'>Login</Button>
-            <Button
-              onClick={ () => this.setState({ showLogin: false })} 
-              bsStyle='link'>Cancel</Button>
-          </Navbar.Form>
+          <Login showLoginFn={this.showLogin}/>
           : 
           <Nav pullRight>
-            <NavItem onClick={() => this.setState({ showLogin: true })}>Login</NavItem>
+            <NavItem onClick={() => this.showLogin() }>Login</NavItem>
           </Nav>
           }
         </Navbar>
