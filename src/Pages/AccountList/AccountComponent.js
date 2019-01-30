dc8b022f6b86e048843ac48920599eafa8ffcc75
jcchoate <jaredchoatepro@gmail.com>
2019-01-30 10:30:10 -0700

    nodemon.json

--- src/Pages/AccountList/AccountComponent.js
+++ src/Pages/AccountList/AccountComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './AccountList.css'
 import littleicon from './../../_ionicons_svg_ios-close-circle-outline.svg'
 import AddBalance from '../HomePage/AddBalance';
+import moment from 'moment'
 
 export default class AccountComponent extends Component {
     state = {
@@ -14,40 +15,41 @@ export default class AccountComponent extends Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-      if(prevState !== this.state.accountInfo) {
-        console.log('updating?')
-      }
+        if (prevState !== this.state.accountInfo) {
+            console.log('updating?')
+        }
     }
 
     render() {
-      let {balances} = this.props
-      let newBalances = [...balances]
-      let currentBalance = newBalances.shift()
-      console.log(currentBalance);
-      let history = balances.map((balance,index)=>{
-        return(
-          <div className="accounthistory">
-          <div key={index + balance}>
-            <h2>{balance.entrydate}</h2>
-            <h2>{balance.balance}</h2>
-          </div>
-          </div>
-        )
+        let { balances } = this.props
+        let newBalances = [...balances]
+        let currentBalance = newBalances.shift()
+        console.log(currentBalance);
+        let history = balances.map((balance, index) => {
+            return (
+                <div className="accounthistory">
+                    <div className="historydisplay" key={index + balance}>
+                        <h2>{moment(balance.entrydate).subtract(6, 'days').calendar()}</h2>
+                        <h2>${balance.balance}</h2>
+                    </div>
+                </div>
+            )
 
-      })
-      console.log(history)
-        
+        })
+        console.log(history)
+
 
         return (
             <div className="all-accounts">
                 <div className="account">
                     <h2>{this.props.name}</h2>
                     <h2>${currentBalance.balance}</h2>
-                    <p>{this.props.date}</p>
                 </div>
                 <div className={this.state.switch ? "history notvisible" : "history"}>
-                    <h3>{history}</h3>
-                <AddBalance accountid={ this.props.accountid}/>
+                    <h3 className={this.state.switch ? "history hidden" : "history"} >{history}</h3>
+                    <div className={this.state.switch ? "history hidden" : "history"}>
+                    <AddBalance accountid={this.props.accountid} />
+                    </div>
                 </div>
                 <img src={littleicon} onClick={() => this.switch()} className={this.state.switch ? "iconx" : "iconx iconactive"} ></img>
             </div>
