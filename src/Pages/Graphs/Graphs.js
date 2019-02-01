44db2df5d3459178c8b3e7eb4a29f6129520037d
mobydickens <steadmegan@gmail.com>
2019-02-01 14:35:14 -0700

    fixed graph styling

--- src/Pages/Graphs/Graphs.js
+++ src/Pages/Graphs/Graphs.js
@@ -1,143 +1,149 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './Graphs.css';
 import axios from 'axios';
 import Plot from 'react-plotly.js';
 import moment from 'moment';
 import { Carousel } from 'react-bootstrap';
 
-class Graphs extends Component{
-    state={
-        accounts:[],
-        balances:[],
-        index: 0,
-        direction: null,
-        total:[]
-    }
+class Graphs extends Component {
+  state = {
+    accounts: [],
+    balances: [],
+    index: 0,
+    direction: null,
+    total: []
+  }
 
-    //grab two arrays from db
-    //one is for account balance, other is for accountDate
+  //grab two arrays from db
+  //one is for account balance, other is for accountDate
 
-    componentDidMount(){
-        axios.get(`/accounts`)
-             .then(res=>{
-                this.setState({accounts: res.data.accounts, balances: res.data.balances})
-             })
-    }
-
-
-    handleSelect = (selectedIndex, e) => {
-      this.setState({
-        index: selectedIndex,
-        direction: e.direction,
-      });
-    }
-
- 
-
-    render(){
+  componentDidMount() {
+    axios.get(`/accounts`)
+      .then(res => {
+        this.setState({ accounts: res.data.accounts, balances: res.data.balances })
+      })
+  }
 
 
-      let displayBalance=this.state.accounts.map((acct,id)=>{
-        let newBalances=this.state.balances.filter((bal,id)=>{
-          if(acct.id===bal.accountid){
-            return true
-          } else {
-            return false
-          }
-        })
-        const x_axis = newBalances.map((val,id)=>{
-          return moment(val.entrydate).format("YYYY MMM Do")
-        })
-        const y_axis = newBalances.map((val,id)=>{
-          return parseFloat(val.balance)
-        })
-        // console.log("new balances", newBalances)
-        
-        return(
-          <div className='accountBalance-parent' key={id}>
+  handleSelect = (selectedIndex, e) => {
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction,
+    });
+  }
 
-                    <Plot 
-                    
-                    data={[
-                      {
-                        //date value from mapped array inside of x
-                        x: x_axis.reverse(),
-                        //specified value from mapped array
-                        y: y_axis,
-                        type: 'scatter',
-                        mode: 'lines+points',
-                        marker: {color: 'green'},
-                      },
-                    ]}
-                    layout={ {width: 1000, height: 650, title: acct.name} }
-                    />
-                </div>
-            )
-          })
 
-          let groupOfAccounts = this.state.accounts.map((acct,id)=>{
-            let newBalances=this.state.balances.filter((bal,id)=>{
-              if(acct.id===bal.accountid){
-                return true
-              } else {
-                return false
-              }
-            })
-            const x_axis = newBalances.map((val,id)=>{
-              return moment(val.entrydate).format("YYYY MMM Do")
-            })
-            const y_axis = newBalances.map((val,id)=>{
-              return parseFloat(val.balance)
-            })
-            // console.log("new balances", newBalances)
-            
-            return{
-                          
-                            //date value from mapped array inside of x
-                            x: x_axis.reverse(),
-                            //specified value from mapped array
-                            y: y_axis,
-                            type: 'scatter',
-                            mode: 'lines+points',
-                            marker: {color: 'green'}
-                          }
-              })
 
-          let grandMasterTotal = 
-          <Carousel.Item>
-            
-            <Plot 
-              data={
-                groupOfAccounts
-              }
-              layout={{width:1000, height: 650, title: 'Total Accounts and Balances'}}
-              />
-              </Carousel.Item>
-          
-          let carouselItems = displayBalance.map((graph, i) => {
-            return(
-              <Carousel.Item key={i} >
-                <div className='landing-carousel' alt="900x500">{graph}</div>
-              </Carousel.Item>
-            )
-          })
+  render() {
 
-          
-          
-          const { index, direction } = this.state;
-          // console.log(displayBalance, "Display balance");
-          return(
-            <div className='graphs-parent'>
-                <Carousel
-                  activeIndex={index}
-                  direction={direction}
-                  onSelect={this.handleSelect}
-                  >
-                  {grandMasterTotal}
-                  {carouselItems}</Carousel>
-            </div>
-        )
-    }
+
+    let displayBalance = this.state.accounts.map((acct, id) => {
+      let newBalances = this.state.balances.filter((bal, id) => {
+        if (acct.id === bal.accountid) {
+          return true
+        } else {
+          return false
+        }
+      })
+      const x_axis = newBalances.map((val, id) => {
+        return moment(val.entrydate).format("YYYY MMM Do")
+      })
+      const y_axis = newBalances.map((val, id) => {
+        return parseFloat(val.balance)
+      })
+      // console.log("new balances", newBalances)
+
+      return (
+        <div className='c-item' key={id}>
+          <Plot
+            data={[
+              {
+                //date value from mapped array inside of x
+                x: x_axis.reverse(),
+                //specified value from mapped array
+                y: y_axis,
+                type: 'scatter',
+                mode: 'lines+points',
+                marker: { color: 'green' },
+              },
+            ]}
+            layout={{ title: 'Total Accounts and Balances', autosize: true }}
+            useResizeHandle={true}
+            style={{ width: "100%", height: "100%" }}
+            config={{ responsive: true }}
+            />
+        </div>
+      )
+    })
+
+    let groupOfAccounts = this.state.accounts.map((acct, id) => {
+      let newBalances = this.state.balances.filter((bal, id) => {
+        if (acct.id === bal.accountid) {
+          return true
+        } else {
+          return false
+        }
+      })
+      const x_axis = newBalances.map((val, id) => {
+        return moment(val.entrydate).format("YYYY MMM Do")
+      })
+      const y_axis = newBalances.map((val, id) => {
+        return parseFloat(val.balance)
+      })
+      // console.log("new balances", newBalances)
+
+      return {
+
+        //date value from mapped array inside of x
+        x: x_axis.reverse(),
+        //specified value from mapped array
+        y: y_axis,
+        type: 'scatter',
+        mode: 'lines+points',
+        marker: { color: 'green' }
+      }
+    })
+
+    let grandMasterTotal =
+      <Carousel.Item>
+        <div className='c-item'>
+          <Plot
+            data={
+              groupOfAccounts
+            }
+            layout={{ title: 'Total Accounts and Balances', autosize: true }}
+            useResizeHandle={true}
+            style={{ width: "100%", height: "100%" }}
+            config={{ responsive: true }}
+          />
+        </div>
+      </Carousel.Item>
+
+    let carouselItems = displayBalance.map((graph, i) => {
+      return (
+        <Carousel.Item key={i} >
+          <div className='landing-carousel' alt="900x500">{graph}</div>
+        </Carousel.Item>
+      )
+    })
+
+
+
+    const { index, direction } = this.state;
+    // console.log(displayBalance, "Display balance");
+    return (
+      <div className='graphs-parent'>
+        <Carousel
+          activeIndex={index}
+          direction={direction}
+          onSelect={this.handleSelect}
+          >
+          {grandMasterTotal}
+          {carouselItems}
+        </Carousel>
+      </div>
+    )
+  }
 }
 
 export default Graphs;
