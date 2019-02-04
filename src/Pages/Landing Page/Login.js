78eb818c1f0e23ca82e2177da37a5f04c3f425ed
jeffrey <jeffrey.nasson@gmail.com>
2019-02-04 12:12:45 -0700

    password

--- src/Pages/Landing Page/Login.js
+++ src/Pages/Landing Page/Login.js
@@ -55,7 +55,7 @@ class Login extends Component {
             <FormControl
               value={ this.state.password }
               onChange={ (e) => this.setState({ password: e.target.value, passwordValidation: null }) }  
-              type="text"
+              type="password"
               onKeyUp={event => {
                 if (event.key === 'Enter') {
                   this.login()
