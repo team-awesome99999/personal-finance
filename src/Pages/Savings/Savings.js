574985775dad8a64d2b6b182d9fcefdc02f27ff8
jeffrey <jeffrey.nasson@gmail.com>
2019-02-06 12:12:38 -0700

    backend setup

--- src/Pages/Savings/Savings.js
+++ src/Pages/Savings/Savings.js
@@ -12,13 +12,6 @@ class Savings extends Component{
         current: null,
         openCalculator: false
     }
-
-    trace1 = {
-        x: [],
-        y: [this.state.total],
-        text: [ 'Horizontal Dashed Line'],
-        mode: 'text'
-      };
   
     openCalculator = () => {
         this.setState({ openCalculator: !this.state.openCalculator })
@@ -31,55 +24,7 @@ class Savings extends Component{
               <SubHeader openCalculator={ this.openCalculator }/>
               { this.state.openCalculator ?
               <Calculator />
-              : null }           
-              <Plot 
-                data={[
-                  this.trace1,
-                  {
-                      //date value from mapped array inside of x
-                      x: [1],
-                      //specified value from mapped array
-                      y: [5],
-                      type: 'bar',
-                      mode: 'Line Dash',
-                      marker: {
-                        line: {
-                          color: 'rgb(231, 99, 250)',
-                          width: '20%'
-                        }, size: 7
-                      },
-                    },
-                ]}
-                layout={{title: 'Vertical and Horizontal Lines Positioned Relative to the Axes',
-                xaxis: {
-                  range: [0, 7]
-                },
-                yaxis: {
-                  range: [0, 10]
-                },
-                width: 500,
-                height: 500,
-                shapes: [
-                  //Line Horizontal
-                  {
-                    type: 'line',
-                    x0: .5,
-                    y0: 10,
-                    x1: 1.5,
-                    y1: 10,
-                    line: {
-                      color: 'rgb(50, 171, 96)',
-                      width: 4,
-                      // dash: 'dashdot'
-                    }
-                  },
-                ]
-              }}
-                useResizeHandle={true}
-                style={{ width: "80%", height: "100%" }}
-                config={{ responsive: true }}
-              />
-
+              : null }       
             </div>
         )
     }
